9e3e589c1e1b68f293f545d885e9e328999cbaa7
Mati <mf.milewski@gmail.com>
2023-11-05 17:54:39 -0300

    Update Property class with the new data

--- utils/property.py
+++ utils/property.py
@@ -1,16 +1,34 @@
 ﻿class Property:
-    def __init__(self, href, price, location, total_surface, covered_surface, rooms, bedrooms, bathrooms, garages,
-                 features=None):
-        self.href = href
+    def __init__(self, url="", price=None, expenses=None, location=None, exact_direction=None, total_surface=None, covered_surface=None, rooms=None, bedrooms=None, bathrooms=None, garages=None, age=None, layout=None, orientation=None):
+        self.reference = url
         self.price = price
+        self.expenses = expenses
         self.location = location
+        self.exact_direction = exact_direction
         self.total_surface = total_surface
         self.covered_surface = covered_surface
         self.rooms = rooms
         self.bedrooms = bedrooms
         self.bathrooms = bathrooms
         self.garages = garages
-        self.features = features or []
+        self.age = age
+        self.layout = layout
+        self.orientation = orientation
 
-    def __str__(self):
-        return f"Property(href={self.href}, price={self.price}, location={self.location}, total_surface={self.total_surface}, covered_surface={self.covered_surface},rooms={self.rooms},bedrooms={self.bedrooms},bathrooms={self.bathrooms},garages={self.garages},features={self.features})"
+    def to_dict(self):
+        return {
+            "Referencia": self.reference,
+            "Price": self.price,
+            "Expenses": self.expenses,
+            "Location": self.location,
+            "Exact Direction": self.exact_direction,
+            "Total Surface": self.total_surface,
+            "Covered Surface": self.covered_surface,
+            "Rooms": self.rooms,
+            "Bedrooms": self.bedrooms,
+            "Bathrooms": self.bathrooms,
+            "Garages": self.garages,
+            "Antiguedad": self.age,
+            "Disposicion": self.layout,
+            "Orientacion": self.orientation,
+        }
